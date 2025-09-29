22fb84817367c27e596d1fa29c01afa97c0fec69
tatazzi <otaviol.gon@gmail.com>
2025-09-29 03:20:38 -0300

    Feat: Add and import SVG from the login screen

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Calendar from "./calendar.svg";
+import EyeOff from "./eye-off.svg";
+import Eye from "./eye.svg";
+import Mail from "./mail.svg";
+import Ticket from "./ticket.svg";
+
+export { Calendar, EyeOff, Eye, Mail, Ticket };
