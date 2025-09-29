e031e81441c6bccdf504695acde4366891d5fbdf
tatazzi <otaviol.gon@gmail.com>
2025-09-29 03:21:23 -0300

    Feat: Create a button component.

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  children: ReactNode;
+  type?: "button" | "submit" | "reset";
+};
+
+export default function Button({ children, type = "button" }: ButtonProps) {
+  return (
+    <button
+      type={type}
+      className="w-full h-12 text-base font-semibold rounded-lg bg-brandPrimary text-white hover:bg-brandPrimary/80 transition-colors duration-200"
+    >
+      {children}
+    </button>
+  );
+}
