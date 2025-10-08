d682c04338606556049569e53c376d47b371b4ea
tatazzi <otaviol.gon@gmail.com>
2025-10-07 23:26:35 -0300

    feat: Add icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,16 @@
 import Calendar from "./calendar.svg";
+import MiniCalendar from "./mini-calendar.svg";
 import EyeOff from "./eye-off.svg";
 import Eye from "./eye.svg";
 import Mail from "./mail.svg";
 import Ticket from "./ticket.svg";
+import UserPlus from "./user-plus.svg";
+import User from "./user.svg";
+import Layers from "./layers.svg";
+import Users from "./users.svg";
+import Plus from "./plus.svg";
+import Edit from "./edit.svg";
+import Trash from "./trash.svg";
+import Coupon from "./coupon.svg";
 
-export { Calendar, EyeOff, Eye, Mail, Ticket };
+export { Calendar, EyeOff, Eye, Mail, Ticket, UserPlus, User, Layers, Users, Plus, Edit, Trash, MiniCalendar, Coupon };
